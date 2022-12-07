ab83fea3a2a80af1fe83e1a3fa126c9742f89469
StanNooijen <Stan.nooijen@gmail.com>
2022-12-07 10:29:30 +0100

    groundCheck

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("MainMenu");
+            Debug.Log("Scene switches");
+        }
     }
 }
