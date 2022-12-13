2185491142ada85749b3db539fc96370b4eaa6e3
StanNooijen <Stan.nooijen@gmail.com>
2022-12-13 16:53:47 +0100

    ball added and sprites added

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -5,12 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    Rigidbody2D rb;
+    public float force = 15f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.one * force;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("");
+            SceneManager.LoadScene("Kevin Scene");
             Debug.Log("Scene switches");
         }
+
+        if (collision.gameObject.CompareTag("WallRight"))
+        {
+            force = -10f;
+            rb.velocity = Vector2.left * force;
+        }
+
+        if (collision.gameObject.CompareTag("WallLeft"))
+        {
+            force = 10f;
+            rb.velocity = Vector2.right * force;
+        }
+
     }
 }
